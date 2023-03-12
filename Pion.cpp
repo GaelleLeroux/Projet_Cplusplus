ebf9e7758d9a354f1c4303be56eff34894ffba25
Capucine <capucine.jumelle@cpe.fr>
2023-03-12 19:39:08 +0100

    pion

--- Pion.cpp
+++ Pion.cpp
@@ -6,7 +6,7 @@ using std::endl;
 //Constructeur de la classe Pion
 Pion::Pion() : position_pion(0) {} //initialisation de la position du piont a 0 losrsque l'objet de tupe Pion est créer.
 
-void Pion::jouer() {
+void Pion::jouer(int M) {
     const int largeur_M = 10; // A modifier quand elle sera faite dans map
     const int hauteur_M = 10; // A modifier quand elle sera faite dans map
     const int derniere_ligne = hauteur_M -1; // indices des ligne de 0 à heuteur-1
